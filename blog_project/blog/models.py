bc15367977a24f59efdb1b7284bc6064b08e801f
Samwuel Simiyu <samwuels003@gmail.com>
2024-11-16 23:32:58 +0300

    Created the Comment model

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Comment(models.Model):
+    '''Handle the comment model'''
+    content = model.TextField()
+    created_at = models.DateTimeField(auto_add_now=True)
+    post = models.ForeignKey(Post, related_name="comments", on_delete=CASCADE)
+    user = models.ForeignKey(User, related_name="comments", on_delete=CASCADE)
+
+    def __str__(self):
+        '''returns the string representation of the comment'''
+        return f"Comment by {self.author.username} on {self.post.title}"
